578ddd18ee24050de54e07624abc9dd9184fa1b8
GabrielKamizato <gabrielkamizato@gmail.com>
2025-08-22 14:37:44 -0300

    exercicio3

---
+++ PMI/Exercicios/exercicios3.py
@@ -0,0 +1,6 @@
+print('-'*50)
+print('Calcule a area do triângulo')
+base = float(input('digite a base: '))
+altura = float(input('digite a altura: '))
+area = (base*altura)/2
+print(f'a area do triângulo é: {area:.2f} m²')
